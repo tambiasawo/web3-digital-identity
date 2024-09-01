06123d2cadf8f209580a186deb6eac2d347a0661
Tambi <tambi.asawo@verticalmotion.ca>
2024-09-01 08:22:50 -0700

    feat: add solidity code

---
+++ utils/index.js
@@ -0,0 +1,17 @@
+import { ethers } from "ethers";
+import identiFi from "./identiFi.json";
+
+export const contract = async () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const { ethereum } = window;
+  if (ethereum) {
+    const signer = provider.getSigner();
+    const contractReader = new ethers.Contract(
+      "0xFd82AefE4eEC40610E479a71f20ab216203AE999",
+      identiFi.abi,
+      signer
+    );
+
+    return contractReader;
+  }
+};
